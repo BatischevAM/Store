af9d9d8590a62ed9aff4130bb082df300941d593
BatischevAM <batischevam@mer.ci.nsu.ru>
2024-09-16 10:12:44 +0700

    Program.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ class Program
             new Meat("Говядина", 500, 1.5),
             new Drink("Кока-Кола", 50, 500),
             new Clothing("Футболка", 300, "M"),
-            new Clothing("Видеокарта", 4000, "4090ti")
+            new Hardware("Видеокарта", 4000, "4090ti")
         };
 
         foreach (Product product in products)
